60c5cfb1826e156dd3e38260c18a4aee459d41ba
Alexandr Kozlov <93259323+AlexBorealis@users.noreply.github.com>
2023-11-29 03:27:42 +0700

    Update main.R

--- main.R
+++ main.R
@@ -36,66 +36,58 @@ main_html <- read_html(for_url[name == "url", value]) |>
 length_list_player <- main_html |>
 	xml_length()
 
+new_player <- map_dfr(1:length_list_player, \(j) {
+			
+			data.table(name_player = name_player(j),
+				   status_player = status_player(j),
+				   position_player = position_player(j),
+				   team = team(j))
+			
+		})[status_player %like% "high level"]
+
 # Main process
 # 1) Checking if an object 'for_bot' exists
 # 2) Checking if an identical object 'for_bot' and last changes fro site
 # 3) If last changes and 'for_bot' is not identical then send message to TG and in DB 
 if (exists("for_bot")) {
 	
-	if (identical(for_bot[status_player %like% "high level"], 
-								map_dfr(1:length_list_player, \(j) {
-									
-									data.table(name_player = name_player(j),
-														 status_player = status_player(j),
-														 position_player = position_player(j),
-														 team = team(j))
-									
-								})[status_player %like% "high level"])) {
+	if (identical(for_bot[status_player %like% "high level"],
+		      new_player)) {
 		
 		NULL
 		
 	} else {
 		
-		new_player <- map_dfr(1:length_list_player, \(j) {
-			
-			data.table(name_player = name_player(j),
-								 status_player = status_player(j),
-								 position_player = position_player(j),
-								 team = team(j))
-			
-		})[status_player %like% "high level"]
-		
 		DT <- data.table(date_observ = strftime(Sys.time(), format = "%Y-%m-%d"),
-										 time_observ = strftime(Sys.time(), format = "%H:%M:%S"),
-										 anti_join(new_player,
-										 					for_bot[status_player %like% "high level"],
-										 					by = c("name_player", "status_player", "position_player", "team")))[!is.na(status_player)]
+				 time_observ = strftime(Sys.time(), format = "%H:%M:%S"),
+				 anti_join(new_player,
+					   for_bot[status_player %like% "high level"],
+					   by = c("name_player", "status_player", "position_player", "team")))[!is.na(status_player)]
 		
 		if (nrow(DT) > 0) {
 			
 			lst <- map(1:nrow(DT), \(i) {
 				
 				bot$sendMessage(chat_id = for_tg[name == "chat_id", value],
-												text = str_glue("{new_player$name_player[i]} {new_player$position_player[i]} ({new_player$team[i]})
-																			
-																			{new_player$status_player[i]}") |> gsub(pattern = "high level - ", replacement = ""))
+						text = str_glue("{new_player$name_player[i]} {new_player$position_player[i]} ({new_player$team[i]})
+								{new_player$status_player[i]}") |> gsub(pattern = "high level - ", replacement = ""))
 				
 			})
 			
 			pool <- dbPool(RPostgreSQL::PostgreSQL(), 
-										 user = for_db[name == "user", value], 
-										 password = for_db[name == "password", value], 
-										 dbname = for_db[name == "dbname", value], 
-										 host = for_db[name == "host", value],
-										 maxSize = 1,
-										 idleTimeout = 1,
-										 validationInterval = 0) # Connect to PostgreSQL
+				       user = for_db[name == "user", value], 
+				       password = for_db[name == "password", value], 
+				       dbname = for_db[name == "dbname", value], 
+				       host = for_db[name == "host", value],
+				       maxSize = 1,
+				       idleTimeout = 1,
+				       validationInterval = 0) # Connect to PostgreSQL
 			
 			dbWriteTable(pool,
-									 value = DT,
-									 name = db_tables$table_name[1],
-									 append = T,
-									 row.names = F)
+				     value = DT,
+				     name = db_tables$table_name[1],
+				     append = T,
+				     row.names = F)
 			
 			poolClose(pool)
 			
@@ -112,9 +104,9 @@ if (exists("for_bot")) {
 	for_bot <- map_dfr(1:length_list_player, \(j) {
 		
 		data.table(name_player = name_player(j),
-							 status_player = status_player(j),
-							 position_player = position_player(j),
-							 team = team(j))
+			   status_player = status_player(j),
+			   position_player = position_player(j),
+			   team = team(j))
 		
 	})
 	
