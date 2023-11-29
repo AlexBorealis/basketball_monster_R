746659d1cd63717b49d8a3e5a185a8e7e52ec2aa
Alexander Kozlov <kozlovaleksandr592@gmail.com>
2023-11-29 19:01:22 +0700

    added options in read_html

--- main.R
+++ main.R
@@ -27,17 +27,11 @@ db_tables <- data.table(read.table("db_tables.txt", header = T))
 bot <- Bot(token = token)
 
 # Read main html
-retry({
-	
-	main_html <- read_html(for_url[name == "url", value]) |>
-		html_elements(xpath = "//*[@class='q-su-holder']")
-	
-}, until = ~ class({
-	
-	read_html(for_url[name == "url", value]) |>
-		html_elements(xpath = "//*[@class='q-su-holder']")
-	
-}) == "xml_nodeset")
+main_html <- read_html(for_url[name == "url", value], 
+											 handle = curl::new_handle("useragent" = "Mozilla/5.0"),
+											 options = c("RECOVER", "PEDANTIC"),
+											 verbose = T) |>
+	html_elements(xpath = "//*[@class='q-su-holder']")
 
 # Getting xml length
 length_list_player <- main_html |>
@@ -46,12 +40,12 @@ length_list_player <- main_html |>
 new_player <- map_dfr(1:length_list_player, \(j) {
 			
 			data.table(name_player = name_player(j),
-				   status_player = status_player(j),
-				   position_player = position_player(j),
-				   team = team(j))
-			
-		})[status_player %like% "high level"] |>
-	distinct(status_player, .keep_all = T)
+							   status_player = status_player(j),
+							   position_player = position_player(j),
+							   team = team(j))
+	
+	})[status_player %like% "high level"] |>
+	distinct(status_player, name_player, .keep_all = T)
 
 # Main process
 # 1) Checking if an object 'for_bot' exists
@@ -59,43 +53,43 @@ new_player <- map_dfr(1:length_list_player, \(j) {
 # 3) If last changes and 'for_bot' is not identical then send message to TG and in DB 
 if (exists("for_bot")) {
 	
-	if (identical(for_bot[status_player %like% "high level"],
-		      new_player)) {
+	if (identical(for_bot[status_player %like% "high level"], new_player)) {
 		
 		NULL
 		
 	} else {
 		
 		DT <- data.table(date_observ = strftime(Sys.time(), format = "%Y-%m-%d"),
-				 time_observ = strftime(Sys.time(), format = "%H:%M:%S"),
-				 anti_join(new_player,
-					   for_bot[status_player %like% "high level"],
-					   by = c("name_player", "status_player", "position_player", "team")))[!is.na(status_player)]
+										 time_observ = strftime(Sys.time(), format = "%H:%M:%S"),
+										 anti_join(new_player,
+										 					 for_bot[status_player %like% "high level"],
+										 					 by = c("name_player", "status_player", "position_player", "team")))[!is.na(status_player)] |>
+			distinct(status_player, name_player, .keep_all = T)
 		
 		if (nrow(DT) > 0) {
 			
 			lst <- map(1:nrow(DT), \(i) {
 				
 				bot$sendMessage(chat_id = for_tg[name == "chat_id", value],
-						text = str_glue("{new_player$name_player[i]} {new_player$position_player[i]} ({new_player$team[i]})
-								{new_player$status_player[i]}") |> gsub(pattern = "high level - ", replacement = ""))
+												text = str_glue("{new_player$name_player[i]} {new_player$position_player[i]} ({new_player$team[i]})
+																				{new_player$status_player[i]}") |> gsub(pattern = "high level - ", replacement = ""))
 				
 			})
 			
 			pool <- dbPool(RPostgreSQL::PostgreSQL(), 
-				       user = for_db[name == "user", value], 
-				       password = for_db[name == "password", value], 
-				       dbname = for_db[name == "dbname", value], 
-				       host = for_db[name == "host", value],
-				       maxSize = 1,
-				       idleTimeout = 1,
-				       validationInterval = 0) # Connect to PostgreSQL
+							       user = for_db[name == "user", value], 
+							       password = for_db[name == "password", value], 
+							       dbname = for_db[name == "dbname", value], 
+							       host = for_db[name == "host", value],
+							       maxSize = 1,
+							       idleTimeout = 1,
+							       validationInterval = 0) # Connect to PostgreSQL
 			
 			dbWriteTable(pool,
-				     value = DT,
-				     name = db_tables$table_name[1],
-				     append = T,
-				     row.names = F)
+							     value = DT,
+							     name = db_tables$table_name[1],
+							     append = T,
+							     row.names = F)
 			
 			poolClose(pool)
 			
@@ -112,10 +106,10 @@ if (exists("for_bot")) {
 	for_bot <- map_dfr(1:length_list_player, \(j) {
 		
 		data.table(name_player = name_player(j),
-			   status_player = status_player(j),
-			   position_player = position_player(j),
-			   team = team(j)) |>
-			distinct(status_player, .keep_all = T)
+						   status_player = status_player(j),
+						   position_player = position_player(j),
+						   team = team(j)) |>
+			distinct(status_player, name_player, .keep_all = T)
 		
 	})
 	
@@ -129,7 +123,7 @@ for_bot <- map_dfr(1:length_list_player, \(j) {
 						 team = team(j))
 	
 }) |>
-	distinct(status_player, .keep_all = T)
+	distinct(status_player, name_player, .keep_all = T)
 
 rm(list = ls() %>% .[. != "for_bot"])
 
