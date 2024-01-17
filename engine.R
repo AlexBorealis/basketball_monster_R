9312f22a5b5a9848f64947906d0f2fcdec59d8b1
Alexander Kozlov <kozlovaleksandr592@gmail.com>
2024-01-18 01:36:36 +0300

    added new status (traded)

--- engine.R
+++ engine.R
@@ -140,7 +140,7 @@ status_player <- function(i = 1) {
 		text_sub_level <- text |>
 			str_replace(pattern = "Started 2nd Half", replacement = toupper("Started 2nd Half "))
 			
-	} else if (!is_empty(grep(text, pattern = "Traded", ignore.case = T)) {
+	} else if (!is_empty(grep(text, pattern = "Traded", ignore.case = T))) {
 
 		text_sub_level <- text |>
 			str_replace(pattern = "Traded", replacement = toupper("Traded "))
